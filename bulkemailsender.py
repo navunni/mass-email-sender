39e2510cbee80d13c9ae33d85471b7fbd8707923
Navaneeth Unnikrishnan <116534904+navunni@users.noreply.github.com>
2022-12-30 17:25:24 -0700

    Update bulkemailsender.py

--- bulkemailsender.py
+++ bulkemailsender.py
@@ -1,3 +1,5 @@
+import csv
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -6,7 +8,21 @@ from email import encoders
 from string import Template
 import pandas as pd
 
-e = pd.read_csv("FILE_NAME.csv")
+e = pd.read_csv("file_name.csv")
 server = smtplib.SMTP(host='smtp.gmail.com', port=587)
 server.starttls()
-server.login('YOUREMAIL@gmail.com','YOURPASSWORD')
+server.login('youremail@gmail.com','yourpassword')
+
+
+body = ("""
+
+Hi there 
+
+Body of the email 
+
+Thank You
+""")
+subject = "Internship Opportunities"
+fromaddr = 'navaneeth12245@gmail.com
+for index, row in e.iterrows()
+  
