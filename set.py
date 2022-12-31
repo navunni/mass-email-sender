1919b7006961012362055fd2f03d51767ef17ba5
Navaneeth Unnikrishnan <116534904+navunni@users.noreply.github.com>
2022-12-31 10:31:04 -0700

    Update and rename bulkemailsender.py to set.py

---
+++ set.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='mass-email-sender',
+   version='1.0',
+   description='Bulk Email Automation',
+   license="MIT",
+   long_description=long_description,
+   author='Navaneeth U.',
+   author_email='nu2071@nyu.edu',
+   url="https://github.com/navunni/mass-email-sender",
+   #packages=['BulkEmail'],
+   #install_requires=['smtplib', 'ssl'], #external packages as dependencies
